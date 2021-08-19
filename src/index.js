bbe9fae0ac08d48a4c6240dd92da9882de456eaf
Faran Saeed <faransaeed@rocketmail.com>
2021-08-20 02:21:58 +0500

    fixed sky-condition font sizes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import 'normalize.css';
-// import './sass/style.scss';
-import { addWeather, resizeCheck } from './modules/dom';
+import './sass/style.scss';
+import addWeather from './modules/dom';
 import updateCard from './modules/functions';
 
 /* default weather card for moscow */
 updateCard('Moscow').then(() => {
   const searchForm = document.querySelector('form');
   searchForm.addEventListener('submit', addWeather);
-  resizeCheck();
 });
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -8,18 +8,4 @@ const addWeather = (event) => {
   updateCard(searchValue);
 };
 
-const resizeCheck = () => {
-  const skyCondition = document.querySelector('#sky-condition > h1');
-  const skyConditionFirstWordLength = skyCondition.innerText.split(' ')[0]
-    .length;
-
-  if (skyConditionFirstWordLength < 8 && screen.width > 570)
-    skyCondition.style.fontSize = '3.5rem';
-  else if (skyConditionFirstWordLength > 8 && screen.width > 570)
-    skyCondition.style.fontSize = '2.4rem';
-  else if (skyConditionFirstWordLength <= 8 && screen.width < 570)
-    skyCondition.style.fontSize = '2rem';
-  else skyCondition.style.fontSize = '1.4rem';
-};
-
-export { addWeather, resizeCheck };
+export default addWeather;
